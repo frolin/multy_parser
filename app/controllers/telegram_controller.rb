b5e4e13262e3807ad05e0b9fd84f0efa4c6bbb48
frolin <fralunia@gmail.com>
2021-07-19 03:38:38 +0300

    try to add telegram bot

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -1,14 +1,14 @@
 class TelegramController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
 
-  # def start!(*)
-  #   respond_with :message, text: t('.content')
-  # end
+  def start!(*)
+    respond_with :message, text: t('.content')
+  end
+
+  def help!(*)
+    respond_with :message, text: t('.content')
+  end
   #
-  # def help!(*)
-  #   respond_with :message, text: t('.content')
-  # end
-  # #
   # def memo!(*args)
   #   if args.any?
   #     session[:memo] = args.join(' ')
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'parsers/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # Most off apps would require
-	telegram_webhook TelegramController
+	telegram_webhook TelegramWebhookController, :multi_parser
 
 end
