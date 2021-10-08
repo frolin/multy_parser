928ce4fefc93cbdf3e42fa974bb986c9dd764006
frolin <fralunia@gmail.com>
2021-10-08 13:57:24 +0300

    alpha-3-find_images headless-browser

--- app/services/parsers/headless_browser.rb
+++ app/services/parsers/headless_browser.rb
@@ -7,11 +7,13 @@ module Parsers
 			# configure the driver to run in headless mode
 			#
 			options = Selenium::WebDriver::Chrome::Options.new
-			# options.add_argument('--headless')
+			options.add_argument('--headless')
       options.add_argument('--enable-javascript')
       options.add_argument('--no-sandbox')
       options.add_argument('--ignore-certificate-errors')
       options.add_argument('--allow-insecure-localhost')
+			options.add_argument("--window-size=1920,1080")
+			options.add_argument("--start-maximized")
 			driver = Selenium::WebDriver.for :chrome, options: options
 
 			driver.get url
