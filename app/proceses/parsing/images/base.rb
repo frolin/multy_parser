a4f17569592f0b6aa65e9d8f353811729126a338
frolin <fralunia@gmail.com>
2021-10-08 13:57:24 +0300

    alpha-3-find_images headless-browser

--- app/proceses/parsing/images/base.rb
+++ app/proceses/parsing/images/base.rb
@@ -13,46 +13,25 @@ class Parsing::Images::Base
 		@agent.get(url)
 	end
 
-	def full_page_get_element(url, element)
-		# https://medium.com/@khaledhassan45/how-to-scrape-dynamic-content-off-of-a-web-page-using-rails-nokogiri-and-watir-dc6275af1e91
-
-		# @browser ||= Watir::Browser.new
-		# @browser.goto(url)
-		#
-		element =
-		binding.pry
-		# html = @browser.element(css: /#{element}/).wait_until(&:present?)
-		# read_html(html, element)
-	end
-
 	def read_html(html, element)
 		page = Nokogiri::HTML(html)
 		page.search(element).attributes['src'].value
 	end
 
 	def url(product_name)
-		@url = "https://yandex.ru/images/search?text=#{product_name}&type=clipart&comm=1&isize=large"
-	end
-
-	def get_image_url(product_name)
-		serp_url =  url(product_name)
-		page(serp_url).search('a.serp-item__link')[3].attributes['href'].value
+		@url = "https://yandex.ru/images/search?text='#{product_name.chomp}'&type=clipart&comm=1&isize=large"
 	end
 
 	def parse!
-
 		@products.each do |product|
-			image_link = get_image_url(product.name)
-			url = 'https://yandex.ru' + image_link
 			next if product.product_image_url.present?
 
-			if image_link.present?
-				image = Parsers::HeadlessBrowser.go_and_find(url, '.MMImage-Origin')
-				next unless image.present?
+			image = Parsers::HeadlessBrowser.new(url(product.name), '.MMImage-Origin').find
+			next unless image.present?
 
-				product.product_image_url = image
-				product.save
-			end
+			product.product_image_url = image
+			product.save
+
 			# Down.download(image.uri, destination: "#{Rails.public_path}/providers/#{provider.slug}")
 		end
 	end
--- app/services/parsers/headless_browser.rb
+++ app/services/parsers/headless_browser.rb
@@ -2,25 +2,28 @@ require "selenium-webdriver"
 
 module Parsers
 	class HeadlessBrowser
+		def initialize(url, element)
+			@url = url
+			@element = element
 
-		def self.go_and_find(url, element)
-			# configure the driver to run in headless mode
-			#
 			options = Selenium::WebDriver::Chrome::Options.new
 			options.add_argument('--headless')
-      options.add_argument('--enable-javascript')
-      options.add_argument('--no-sandbox')
-      options.add_argument('--ignore-certificate-errors')
-      options.add_argument('--allow-insecure-localhost')
+			options.add_argument('--enable-javascript')
+			options.add_argument('--no-sandbox')
+			options.add_argument('--ignore-certificate-errors')
+			options.add_argument('--allow-insecure-localhost')
 			options.add_argument("--window-size=1920,1080")
 			options.add_argument("--start-maximized")
-			driver = Selenium::WebDriver.for :chrome, options: options
+			@driver = Selenium::WebDriver.for :chrome, options: options
+			@wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+		end
 
-			driver.get url
-			wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-			element = wait.until { driver.find_element(css: element) }
+		def find
+			@driver.get @url
+			@driver.find_element(css: 'a.serp-item__link').click
 
-			element.attribute('src') if element.present?
+			image = @wait.until { @driver.find_element(css: @element) }
+			image.attribute('src')
 		end
 	end
 end
